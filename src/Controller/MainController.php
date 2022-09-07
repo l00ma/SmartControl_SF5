367c26eb5118ad206720b29ef56837717d0b6d1d
l00ma <looma@protonmail.ch>
2022-09-08 00:15:17 +0200

    feat: ajout graph motion

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,4 +15,16 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
+    #[Route('/welcome/motion', name: 'motion')]
+    public function motion(): Response
+    {
+        if (file_exists($this->getParameter('data_directory') . '/pir_sensor.data')) {
+            $motion_stat = file_get_contents($this->getParameter('data_directory') . '/pir_sensor.data');
+
+            return $this->render('main/motion.html.twig', [
+                'motion_stat' => $motion_stat
+            ]);
+        }
+    }
 }
