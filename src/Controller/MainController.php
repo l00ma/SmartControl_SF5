ffc21d588e164c543c8a88de308b17e31c345215
l00ma <looma@protonmail.ch>
2022-10-06 21:52:35 +0200

    feat: temperatures charts

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -44,7 +44,7 @@ class MainController extends AbstractController
     }
 
     #[Route('/motion/load', name: 'motion_load')]
-    public function load(): JsonResponse
+    public function m_load(): JsonResponse
     {
         $refresh = $this->getUser()->getMouvementPir()->getGraphRafraich();
         $cam = $this->getUser()->getMouvementPir()->getEnreg();
@@ -55,7 +55,7 @@ class MainController extends AbstractController
     }
 
     #[Route('/motion/save', name: 'motion_save', methods: 'GET|POST')]
-    public function save(Request $request)
+    public function m_save(Request $request)
     {
         $member = $this->getUser();
 
@@ -68,5 +68,26 @@ class MainController extends AbstractController
         $this->em->flush();
 
         return $this->render('main/motion.html.twig');
+    }
+
+    #[Route('/temp', name: 'temp')]
+    public function temp(): Response
+    {
+
+        return $this->render('main/temp.html.twig');
+    }
+
+    #[Route('/temp/load', name: 'temp_load')]
+    public function t_load()
+    {
+        $fileContent_int = file_get_contents($this->getParameter('data_directory') . '/temp_sensor.data');
+        $fileContent_int = '{"data_int":' . $fileContent_int . ',';
+        $fileContent_ext = file_get_contents($this->getParameter('data_directory') . '/temp_ext_sensor.data');
+        $fileContent_ext = ' "data_ext":' . $fileContent_ext . ',';
+        $fileContent_bas = file_get_contents($this->getParameter('data_directory') . '/temp_bas_sensor.data');
+        $fileContent_bas = ' "data_bas":' . $fileContent_bas . '}';
+        $fileContent = $fileContent_int . $fileContent_ext . $fileContent_bas;
+        $fileContent = json_encode($fileContent);
+        return new Response($fileContent);
     }
 }
