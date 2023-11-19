f042d5ccbe3a320dbf6911b79af9fe5eaf2aeae4
l00ma <looma@protonmail.ch>
2023-11-19 18:21:38 +0100

    jquery replaced with js in all scripts

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,4 +1,5 @@
 import "./player";
+import {forEach} from "core-js/stable/dom-collections";
 
 function loadDatas() {
 	fetch('gallery/discusage')
@@ -21,19 +22,25 @@ function parseAndDisplay(data) {
 	let totalSpace, freeSpace, usedRate;
 	[totalSpace, freeSpace, usedRate] = data;
 
-	$("#bar").html('<div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="' + usedRate + '" aria-valuemin="0" aria-valuemax="100" style="height: 15px" title="Espace disque total: ' + totalSpace + ' Go,&#10;Espace disque dispo: ' + freeSpace + ' Go"> <div class="progress-bar bg-success overflow-visible text-light petite" style="width: ' + usedRate + '%">' + usedRate + '%</div></div> <div class="xpetite">Espace disque utilisé</div> ');
+	document.getElementById("bar").innerHTML = '<div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="' + usedRate + '" aria-valuemin="0" aria-valuemax="100" style="height: 15px" title="Espace disque total: ' + totalSpace + ' Go,&#10;Espace disque dispo: ' + freeSpace + ' Go"> <div class="progress-bar bg-success overflow-visible text-light petite" style="width: ' + usedRate + '%">' + usedRate + '%</div></div> <div class="xpetite">Espace disque utilisé</div> ';
 }
 
 //action au chargement de la page
 document.addEventListener("DOMContentLoaded", function () {
 	loadDatas();
 	//action button tout selectionner
-	$('#but_all').click(function () {
-		$('#tableau div').find('input:checkbox').prop('checked', true);
+	document.getElementById("but_all").addEventListener("click", function (){
+		forEach.call( document.querySelectorAll('input[type="checkbox"]'),function(el){
+				el.checked=true;
+			}
+		);
 	});
 
 	//action button tout déselectionner
-	$('#but_none').click(function () {
-		$('#tableau div').find('input:checkbox').prop('checked', false);
+	document.getElementById("but_none").addEventListener("click", function (){
+		forEach.call( document.querySelectorAll('input[type="checkbox"]'),function(el){
+				el.checked=false;
+			}
+		);
 	});
 });
